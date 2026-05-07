b4f2d9d309236c04b3296f88bfdbdecf165ebef7
Abdul Moiz Solaiman <abdul.solaiman@accessgroup.net.au>
2026-05-07 13:35:48 +0800

    chore: update whitepaces

--- app/page.tsx
+++ app/page.tsx
@@ -19,21 +19,22 @@ export default function Home() {
             To get started, edit the page.tsx file.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
+            Looking for a starting point or more instructions? Head over
+            to&nbsp;
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-900 dark:text-zinc-50"
             >
               Templates
-            </a>{" "}
-            or the{" "}
+            </a>
+            &nbsp; or the&nbsp;
             <a
               href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-900 dark:text-zinc-50"
             >
               Learning
-            </a>{" "}
-            center.
+            </a>
+            &nbsp;center.
           </p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
